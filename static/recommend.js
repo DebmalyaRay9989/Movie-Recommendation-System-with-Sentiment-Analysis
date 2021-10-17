068d40c1e9b4bb3093c3bf3715b9fcb6d132c5ca
Debmalya Ray <66347154+DebmalyaRay9989@users.noreply.github.com>
2021-10-17 15:45:47 +0530

    commit
    commit

--- static/recommend.js
+++ static/recommend.js
@@ -12,7 +12,7 @@ $(function() {
   source.addEventListener('input', inputHandler);
 
   $('.movie-button').on('click',function(){
-    var my_api_key = 'YOUR_API_KEY';
+    var my_api_key = '9e2f944f4f961b458faff4ed349ac453';
     var title = $('.movie').val();
     if (title=="") {
       $('.results').css('display','none');
@@ -26,7 +26,7 @@ $(function() {
 
 // will be invoked when clicking on the recommended movies
 function recommendcard(e){
-  var my_api_key = 'YOUR_API_KEY';
+  var my_api_key = '9e2f944f4f961b458faff4ed349ac453';
   var title = e.getAttribute('title'); 
   load_details(my_api_key,title);
 }
